a54e956c86f4b2f8e3ff9b7fd5033b0258579176
Renato Lopes dos Santos <renatolsantos@hotmail.com>
2019-09-14 10:20:38 -0300

    Adicionar arquivos de projeto.

---
+++ Game Teste - v1.0 - API/Process/GameProcess.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Game_Teste___v1._0___Controller.Process
+{
+
+    [Route("api/")]
+    [ApiController]
+    public class GameCProcess : ControllerBase
+    {
+        public ActionResult Teste()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Game Teste - v1.0 - Controller/Controllers/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Game_Teste___v1._0___Controller.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult Game()
+        {
+            return View();
+        }
+    }
+}
